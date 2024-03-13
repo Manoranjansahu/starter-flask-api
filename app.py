6b358ea4e593df3c9d4b1a212631db1a164d4068
Manoranjansahu <96382266+Manoranjansahu@users.noreply.github.com>
2024-03-13 12:05:48 +0530

    app.py

--- app.py
+++ app.py
@@ -1,32 +1,10 @@
-import paho.mqtt.client as mqtt
-from flask import Flask, jsonify
-import os
-from threading import Thread
+from flask import Flask, render_template
 
 app = Flask(__name__)
-mqtt_broker = "mqtt.eclipseprojects.io"
-mqtt_topic = "TEMPERATURE"
-message = ""
 
 @app.route('/')
 def index():
-    global message
-    return f"{message}"
-
-def on_message(client, userdata, msg):
-    global message
-    message = msg.payload.decode('utf-8')
-
-def thread():
-    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-    client.on_message = on_message
-    client.connect(mqtt_broker, port = 80)
-
-    client.subscribe("TEMPERATURE")
-    client.loop_forever()
-
+    return render_template("subscriber.html")
 
 if __name__ == '__main__':
-    t1 = Thread(target = thread)
-    t1.start()
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run()
