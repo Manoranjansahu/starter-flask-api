ac6048bdede95e990b580c5aa84e710ff7dcc337
Manoranjansahu <96382266+Manoranjansahu@users.noreply.github.com>
2024-04-05 00:03:22 +0530

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,49 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+import urllib.request as rrequest
+import json
+import re
+from time import sleep
+import multiprocessing
 
 app = Flask(__name__)
+def schedule_task(delay):
+    sleep(delay)
+    _data = {
+            "chat_id": 1069737157,
+            "text": "thread"
+        }
+    _request = rrequest.Request("https://api.telegram.org/bot6531655799:AAFuGpdFGZRfN7ndifFJwInv_rIPoyIa27s/sendMessage", data=json.dumps(_data).encode('utf8'), headers={"Content-Type": "application/json"})
+    rrequest.urlopen(_request)
+    
 
 @app.route('/')
 def index():
     return render_template("publisher.html")
+
+@app.route('/send/<msg>')
+def send(msg):
+    _data = {
+            "chat_id": 1069737157,
+            "text": str(msg)
+        }
+    _request = rrequest.Request("https://api.telegram.org/bot6531655799:AAFuGpdFGZRfN7ndifFJwInv_rIPoyIa27s/sendMessage", data=json.dumps(_data).encode('utf8'), headers={"Content-Type": "application/json"})
+    rrequest.urlopen(_request)
+    return "ok"
+
+@app.route('/telegram')
+def send(msg):
+    if __name__ == '__main__':
+        p = multiprocessing.Process(target=schedule_task, args=(5,))
+        p.start()
+    _data = {
+            "chat_id": 1069737157,
+            "text": "hi"
+        }
+    _request = rrequest.Request("https://api.telegram.org/bot6531655799:AAFuGpdFGZRfN7ndifFJwInv_rIPoyIa27s/sendMessage", data=json.dumps(_data).encode('utf8'), headers={"Content-Type": "application/json"})
+    rrequest.urlopen(_request)
+    return "ok"
 
 if __name__ == '__main__':
     app.run()
